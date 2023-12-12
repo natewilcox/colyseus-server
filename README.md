a448c450ab22750310108412ac8e6cdebcbb6464
natewilcox@gmail.com <natewilcox@gmail.com>
2023-12-11 20:55:49 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# colyseus_server
+# Colyseus Server
+
+This is a multiplayer game server built with [Colyseus](https://colyseus.io/), a multiplayer game server for Node.js.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+- Node.js
+- npm
+
+### Installing
+
+1. Clone the repository
+```bash
+git clone https://github.com/yourusername/colyseus-server.git
---
+++ src/app.config.ts
@@ -0,0 +1,61 @@
+import config from "@colyseus/tools";
+import { monitor } from "@colyseus/monitor";
+import { playground } from "@colyseus/playground";
+import { PrivateRoom, PublicRoom } from "@natewilcox/tic-tac-toe-server";
+import * as PICOW from "@natewilcox/picow-server";
+import { GameRoom } from "@natewilcox/rabit-jump-server";
+import { GameRoom as ZeldaRoom } from "@natewilcox/zelda-server";
+
+export default config({
+
+    initializeGameServer: (gameServer) => {
+ 
+        console.log('Configuring TIC-TAC-TOE rooms....');
+        gameServer.define('tictactoe_public_room', PublicRoom);
+        gameServer.define('tictactoe_private_room', PrivateRoom);
+
+        console.log('Configuring RABIT-JUMP rooms....');
+        gameServer.define('rabit_room', GameRoom);
+        
+        console.log('Configuring PICOW rooms....');
+        gameServer.define('picow_public_room', PICOW.PublicRoom);
+        gameServer.define('picow_private_room', PICOW.PrivateRoom);
+
+        console.log('Configuring ZELDA rooms...');
+        gameServer.define('zelda_room', ZeldaRoom);
+
+        console.log("READY!!!!!");
+    },
+
+    initializeExpress: (app) => {
+        /**
+         * Bind your custom express routes here:
+         * Read more: https://expressjs.com/en/starter/basic-routing.html
+         */
+        app.get("/hello_world", (req, res) => {
+            res.send("It's time to kick ass and chew bubblegum!");
+        });
+
+        /**
+         * Use @colyseus/playground
+         * (It is not recommended to expose this route in a production environment)
+         */
+        // if (process.env.NODE_ENV !== "production") {
+        //     app.use("/", playground);
+        // }
+
+        /**
+         * Use @colyseus/monitor
+         * It is recommended to protect this route with a password
+         * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
+         */
+        //app.use("/colyseus", monitor());
+    },
+
+
+    beforeListen: () => {
+        /**
+         * Before before gameServer.listen() is called.
+         */
+    }
+});
